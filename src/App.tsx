ae8707250e13f96e027b92c91dbbf415861a87d3
GHOST <maam98617@gmail.com>
2026-07-06 01:38:27 +0330

    Add gh-pages config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// === Use this line for standard starting (without GitHub Pages): ===
+
 import { LanguageProvider } from "./context/LanguageContext";
 import { HomePage } from "./pages/HomePage";
 import { EpisodePage } from "./pages/EpisodesPage";
@@ -7,13 +11,18 @@ import { ScrollToTop } from "./components/scrollToTop";
 function App() {
     return (
         <LanguageProvider>
-            <BrowserRouter>
+            <HashRouter>
+                {/* <BrowserRouter> */}
+                {/* For normal mode ^^ */}
+
                 <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/episodes" element={<EpisodePage />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
+
+            {/* </BrowserRouter> */}
         </LanguageProvider>
     )
 }
